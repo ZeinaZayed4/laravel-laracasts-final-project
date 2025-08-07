69a94e7559004a3406c8451060355d15b65d181a
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-08-08 00:30:32 +0300

    Day 28: blade and tailwind techniques for your Laravel views

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./**/*.blade.php'],
-    darkMode: 'class',
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js"
+    ],
     theme: {
         extend: {
             colors: {
-                "black": "red"
+                "black": "#060606"
+            },
+            fontFamily: {
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
+            },
+            fontSize: {
+                "2xs": ".625rem" // 10px
             }
         },
     },
     plugins: [],
-};
+}
