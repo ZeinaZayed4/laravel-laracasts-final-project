846721da1c8949e986b136da0f7f1e2bf8d69068
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-08-10 00:01:27 +0300

    Day 30

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,3 +1,3 @@
-@props(['width' => 90])
+@props(['employer', 'width' => 100])
 
-<img src="https://picsum.photos/seed/{{ rand(0, 10000) }}/{{ $width }}" alt="" class="rounded-xl">
+<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}">
